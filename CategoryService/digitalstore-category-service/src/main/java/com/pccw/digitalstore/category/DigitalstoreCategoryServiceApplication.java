7d0c5bc3dddcdf986d9c0121be5b005230a6c582
Paul Lee <paulearnelb.lee@pccw.com>
2018-04-20 16:03:58 +0800

    Fresh spring boot, Initial commit

---
+++ CategoryService/digitalstore-category-service/src/main/java/com/pccw/digitalstore/category/DigitalstoreCategoryServiceApplication.java
@@ -0,0 +1,12 @@
+package com.pccw.digitalstore.category;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalstoreCategoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalstoreCategoryServiceApplication.class, args);
+	}
+}
