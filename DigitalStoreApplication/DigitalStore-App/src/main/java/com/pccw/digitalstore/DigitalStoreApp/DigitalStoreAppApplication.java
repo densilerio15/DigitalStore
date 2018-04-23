f2b57e3135da2f423620dd87cc52650fcdaf09fe
81100190 <81100190@PC81100190.corp.root>
2018-04-23 11:08:44 +0800

    Added DigitalStoreApplication

---
+++ DigitalStoreApplication/DigitalStore-App/src/main/java/com/pccw/digitalstore/DigitalStoreApp/DigitalStoreAppApplication.java
@@ -0,0 +1,12 @@
+package com.pccw.digitalstore.DigitalStoreApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalStoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalStoreAppApplication.class, args);
+	}
+}
